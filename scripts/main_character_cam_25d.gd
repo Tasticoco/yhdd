de4d576ccde507205c7266308b72ba74430e9c20
Tasticoco <arthurjunod@gmail.com>
2026-05-29 02:25:28 +0200

    feat : add 2 navigable scenes

--- scripts/main_character_cam_25d.gd
+++ scripts/main_character_cam_25d.gd
@@ -1,23 +1,23 @@
 extends CharacterBody3D
 
+@export var cam : Camera3D
+@onready var sprite_3d: Sprite3D = $Sprite3D
+@onready var collision_shape_3d: CollisionShape3D = $CollisionShape3D
 
-const SPEED : float = 5.0
+const SPEED : float = 7.0
 const JUMP_VELOCITY : float = 4.5
 
+func _ready() -> void:
+	sprite_3d.look_at(cam.global_position)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
+	
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir := Input.get_vector("left", "right", "up", "down")
+	var direction := (cam.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
